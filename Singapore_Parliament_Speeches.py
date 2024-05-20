4fa123d22062809cbfb825a558ddfe24fed17b18
jeremychia <jeremy.chia@vinted.com>
2024-05-20 13:54:42 +0200

    refactor: put to utils

--- Singapore_Parliament_Speeches.py
+++ Singapore_Parliament_Speeches.py
@@ -1,27 +1,10 @@
 # streamlit_app.py
 
 import streamlit as st
+from utils import project_id, run_query
 from millify import millify
-from google.oauth2 import service_account
-from google.cloud import bigquery
-import pandas as pd
 
 ### BACKEND
-
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-project_id = 'singapore-parliament-speeches'
-
-@st.cache_data(ttl=6000)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
 
 def get_dataset_overview():
     query = f"""
---
+++ utils/__init__.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import pandas as pd
+
+# Create API client.
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+project_id = 'singapore-parliament-speeches'
+
+@st.cache_data(ttl=6000)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
