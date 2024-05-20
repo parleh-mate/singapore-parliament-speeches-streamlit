5ae06ef849a929cd516cdfe2a923e7bffb39355e
jeremychia <jeremy.chia@vinted.com>
2024-05-20 14:24:16 +0200

    chore: add support page

---
+++ pages/7_Spot_an_Issue?.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('Report Issues')
+
+st.markdown("""
+            * You see an issue?
+            * You would like to request for a feature?""")
+st.divider()
+st.write("Please get in contact with us at singapore.parliament.speeches@gmail.com.")
